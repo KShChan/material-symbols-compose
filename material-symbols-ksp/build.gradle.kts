1d78b62427ab7dde83f3f451e288db2ea6765d86
1552980358 <1552980358@qq.com>
2025-02-17 18:03:29 +0800

    MaterialSymbolsCompose: Enable source jar publishing

--- material-symbols-ksp/build.gradle.kts
+++ material-symbols-ksp/build.gradle.kts
@@ -6,6 +6,9 @@ plugins {
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+
+    withJavadocJar()
+    withSourcesJar()
 }
 
 dependencies {
